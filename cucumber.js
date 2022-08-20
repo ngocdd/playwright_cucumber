cb1c2fd3faa6ee5c5f625e3ef7080849a1d81243
Oliver-Dang Dinh Ngoc <dinhngoc.dang@manabie.com>
2022-08-20 15:49:26 +0700

    update gherkin lint

--- cucumber.js
+++ cucumber.js
@@ -1,10 +1,10 @@
 module.exports = {
-    default: {
-        parallel: 1,
-        // format: ['html:cucumber-report.html'],
-        paths: ['features/**/*.feature'], // path for feature files
-        requireModule: ['ts-node/register'],
-        require: ['src/support/common-hooks.ts', 'src/step-definitions/**/*.ts'], // path for step definitions,
-        publishQuiet: true,
-    },
+  default: {
+    parallel: 1,
+    // format: ['html:cucumber-report.html'],
+    paths: ['features/**/*.feature'], // path for feature files
+    requireModule: ['ts-node/register'],
+    require: ['support/common-hooks.ts', 'step-definitions/**/*.ts'], // path for step definitions,
+    publishQuiet: true,
+  },
 };
--- features/homepage.feature
+++
@@ -1,8 +0,0 @@
-Feature: Random
-  A random feature using some Playwright stuff
-
-  @ignore
-  Scenario: test home page
-    Given go to google page
-    When search ngocdd
-    Then see ngocdd in result page
