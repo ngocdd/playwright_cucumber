a019c2eda9e5182be00ca06c0f0256be98b2a8df
Oliver-Dang Dinh Ngoc <dinhngoc.dang@manabie.com>
2022-08-25 17:23:33 +0700

    update steps definition

--- step-definitions/element-textbox-definition.ts
+++ step-definitions/element-textbox-definition.ts
@@ -1,38 +1,9 @@
-import { Given, When, Then } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
+import { elementPage } from './selectors/element-textbox-locators';
 import { OurWorld } from '../support/world';
 
-Given('go to demoqa site', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
+async function inputFullName(this: OurWorld, name: string) {
+  const page = this.page;
+  await page.fill(elementPage.fullName, name);
+}
 
-Given('Go to element page', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
-
-Given('go to textbox page', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
-
-Given('Input {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
-
-Given('Input {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
-
-When('User submit', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
-
-Then('User sees submit is successfully', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
+export {};
---
+++ step-definitions/element-textbox-steps.ts
@@ -0,0 +1,48 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+import { expect } from '@playwright/test';
+import { OurWorld } from '../support/world';
+import {}
+
+Given('go to demo site', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('Go to element page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('go to textbox page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('Input {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('Input {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('User submit', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('User sees submit is successfully', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('Go to demoqa site', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+Given('Go to textbox page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
