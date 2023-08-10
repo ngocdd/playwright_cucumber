98859057875308e45239bdd51877ac9029fc2751
Oliver-Dang Dinh Ngoc <107824423+ngocdd@users.noreply.github.com>
2023-08-10 09:16:11 +0700

    Update login-to-cms.ts

--- step-definitions/definition/login-to-cms.ts
+++ step-definitions/definition/login-to-cms.ts
@@ -4,9 +4,9 @@ import { OurWorld } from '../../support/world';
 export const login = async (driver: OurWorld, orgID: string, usrName: string, passWord: string) => {
   const page = driver.page;
   console.log('haha');
-  await page.goto('https://administration.manabie.net/');
-  await page.fill(orgID, 'synersia');
-  await page.fill(usrName, 'phuc.chau+e2etokyoschooladmin@manabie.com');
-  await page.fill(passWord, 'Manabie123');
+  await page.goto('');
+  await page.fill(orgID, '');
+  await page.fill(usrName, ');
+  await page.fill(passWord, '');
   await page.click(submitButton);
 };
